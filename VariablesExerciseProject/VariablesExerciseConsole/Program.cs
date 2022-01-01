c4e20657fee11aed8d836f4d163f6cfcf9ae0385
jcrowe91 <jcrowe9119@gmail.com>
2022-01-01 13:37:27 -0600

    Finished Project

---
+++ VariablesExerciseProject/VariablesExerciseConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariablesExerciseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jake";
+            int age = 30;
+            char number = (char)12;
+            bool isHuman = true;
+            double numOfCars = 3.0;
+            decimal pi = 3.14m;
+            
+
+            Console.WriteLine($"My name is {name} and I am {age} years old. I have {numOfCars} cars in my yard.");
+        }
+    }
+}
